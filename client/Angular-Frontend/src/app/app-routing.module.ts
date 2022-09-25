b374782fb2822933a09bdf409a9e1aeb9891a5cd
Utkarshg25 <utkarshgangwar909@gmail.com>
2022-09-25 23:51:32 +0530

    add routing module

--- client/Angular-Frontend/src/app/app-routing.module.ts
+++ client/Angular-Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-
+const routes : Routes =[
+  {
+    path:'employees' , component:EmployeeListComponent
+  },
+  {path:'', redirectTo:'employees' , pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
